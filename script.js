b38fca71545636fb43a533e773124385a245b9b4
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-05-28 08:35:15 -0700

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,16 @@ describe('Signup Modal', () => {
     // Visit the page
     cy.visit('http://localhost:3000');
 
-    // Click the first button (Sign Up button)
-    cy.get('button').first().click();
+    // Click the "Sign Up" button
+    cy.get('button').contains('Sign Up').click();
 
     // Check that the modal is visible
     cy.get('#signupModal').should('be.visible');
 
     // Click the close button
-    cy.get('.close').click();
+    cy.get('#signupModal .close').click();
 
     // Check that the modal is not visible
     cy.get('#signupModal').should('not.be.visible');
-	  cy.get('.close').click();
-cy.wait(500); // Wait for 500ms
-cy.get('#signupModal').should('not.be.visible');
   });
 });
