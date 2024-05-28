5a7beb5115583f59aeaf7c0393c98fafc8808f17
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-05-28 08:59:16 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-describe('Signup Modal', () => {
-  it('should display the modal when the "Sign Up" button is clicked and hide when the close button is clicked', () => {
-    // Visit the page
-    cy.visit('http://localhost:3000');
+// describe('Signup Modal', () => {
+//   it('should display the modal when the "Sign Up" button is clicked and hide when the close button is clicked', () => {
+//     // Visit the page
+//     cy.visit('http://localhost:3000');
 
-    // Click the "Sign Up" button
-    cy.get('button').contains('Sign Up').click();
+//     // Click the "Sign Up" button
+//     cy.get('button').contains('Sign Up').click();
 
-    // Check that the modal is visible
-    cy.get('#signupModal').should('be.visible');
-// Click the close button
-cy.get('.close').click();
+//     // Check that the modal is visible
+//     cy.get('#signupModal').should('be.visible');
+// // Click the close button
+// cy.get('.close').click();
 
-// Check that the modal is not visible
-cy.get('.modal').should('not.be.visible').then(() => {}, {timeout: 10000}); // Wait for 10 seconds
-    });
+// // Check that the modal is not visible
+// cy.get('.modal').should('not.be.visible').then(() => {}, {timeout: 10000}); // Wait for 10 seconds
+//     });
+// });
+it('tests modal functionality', () => {
+  cy.get('button').first().click();
+  cy.get('.modal').should('be.visible');
+  cy.get('.close').click();
+  cy.get('.modal').should('not.be.visible');
 });
